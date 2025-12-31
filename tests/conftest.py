bb22400847d919acd8e49e13abda66d4a2c521da
Roeland Wiersema <roel.wier@proton.me>
2025-12-31 03:36:29 -0500

    Compile and build local and non-local cuda. only test potrs

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+def pytest_collection_modifyitems(config, items):
+    selected = []
+    deselected = []
+    for item in items:
+        if "spmd/test_potrs.py" in item.nodeid:
+            selected.append(item)
+        else:
+            deselected.append(item)
+    if deselected:
+        config.hook.pytest_deselected(items=deselected)
+    items[:] = selected
