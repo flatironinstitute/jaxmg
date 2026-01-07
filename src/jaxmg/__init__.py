2729a6b5dcb3be6dc98d588d1711668853dba78b
Roeland Wiersema <roel.wier@proton.me>
2026-01-07 10:43:20 -0500

    Trigger jenkins

--- src/jaxmg/__init__.py
+++ src/jaxmg/__init__.py
@@ -36,6 +36,7 @@ def _load(module, libraries):
                 ) from e
 
 import jax
+
 # Only import libraries for GPU compatible JAX
 if any("gpu" == d.platform for d in jax.devices()):
     import jax.extend
