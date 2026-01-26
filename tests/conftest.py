98b974b2b83ca16e5e9b4f50faaaed8fb59feb31
Roeland Wiersema <roel.wier@proton.me>
2026-01-26 12:42:03 -0500

    skip cpu mpmd test

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,10 @@ def pytest_collection_modifyitems(config, items):
     deselected = []
     for item in items:
         selected.append(item)
-        # if "mpmd/test_launch_potrs.py" in item.nodeid:
-        #     selected.append(item)
-        # else:
-        #     deselected.append(item)
+        if "mpmd/test_launch_2d_grid_reduce.py" in item.nodeid:
+            selected.append(item)
+        else:
+            deselected.append(item)
     if deselected:
         config.hook.pytest_deselected(items=deselected)
     items[:] = selected
