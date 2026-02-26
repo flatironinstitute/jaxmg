a1c7ec9b589cfd980aa91bb44867b001f8f19f45
Roeland Wiersema <roel.wier@proton.me>
2026-02-26 10:00:31 -0500

    fix error

--- src/jaxmg/_syevd.py
+++ src/jaxmg/_syevd.py
@@ -126,7 +126,7 @@ def syevd(
     assert a.ndim == 2, "a must be a 2D array."
     if T_A > 1024:
         raise ValueError(
-            "T_A has a maximum value of 1024 for SyevdMg, received T_A={T_A}"
+            f"T_A has a maximum value of 1024 for SyevdMg, received T_A={T_A}"
         )
     axis_name = in_specs._partitions[0]
     N_rows, N = a.shape
