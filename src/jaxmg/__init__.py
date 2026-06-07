3dda56b43f5eafe9e6d65db6fc6f98308a92505e
Jacob Tutt <jacobtutt@icloud.com>
2026-06-07 17:24:25 +0100

    Export XLA communicator helpers

--- src/jaxmg/__init__.py
+++ src/jaxmg/__init__.py
@@ -1,6 +1,22 @@
 from ._potrs import potrs, potrs_shardmap_ctx
 from ._potri import potri, potri_shardmap_ctx, potri_symmetrize
 from ._syevd import syevd, syevd_shardmap_ctx
+from ._xla_comm_probe import (
+    xla_comm_collective_probe,
+    xla_comm_collective_probe_shardmap,
+    xla_comm_allreduce_probe,
+    xla_comm_allreduce_probe_shardmap,
+    xla_comm_ring_permute_probe,
+    xla_comm_ring_permute_probe_shardmap,
+    xla_comm_shift_permute_probe,
+    xla_comm_shift_permute_probe_shardmap,
+    xla_comm_permute_probe,
+    xla_comm_permute_probe_shardmap,
+    xla_comm_chunk_permute_probe,
+    xla_comm_chunk_permute_probe_shardmap,
+    xla_comm_matrix_column_native_plan,
+    xla_comm_matrix_column_native_plan_shardmap,
+)
 from ._cyclic_1d import (
     cyclic_1d,
     calculate_padding,
@@ -19,6 +35,20 @@ __all__ = [
     "potri_symmetrize",
     "syevd",
     "syevd_shardmap_ctx",
+    "xla_comm_collective_probe",
+    "xla_comm_collective_probe_shardmap",
+    "xla_comm_allreduce_probe",
+    "xla_comm_allreduce_probe_shardmap",
+    "xla_comm_ring_permute_probe",
+    "xla_comm_ring_permute_probe_shardmap",
+    "xla_comm_shift_permute_probe",
+    "xla_comm_shift_permute_probe_shardmap",
+    "xla_comm_permute_probe",
+    "xla_comm_permute_probe_shardmap",
+    "xla_comm_chunk_permute_probe",
+    "xla_comm_chunk_permute_probe_shardmap",
+    "xla_comm_matrix_column_native_plan",
+    "xla_comm_matrix_column_native_plan_shardmap",
     "cyclic_1d",
     "pad_rows",
     "unpad_rows",
