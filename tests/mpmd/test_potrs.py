8e3fa944ff58253b95f32dc7c9e62068af1f75c1
Jacob Tutt <jacobtutt@icloud.com>
2026-06-08 14:51:32 +0100

    Add 4-GPU MPMD potrs test hook

--- tests/mpmd/test_potrs.py
+++ tests/mpmd/test_potrs.py
@@ -8,3 +8,8 @@ from mpmd_helper import run_mpmd_script
 @pytest.mark.mpmd
 def test_potrs_2gpu():
     run_mpmd_script(Path(__file__).with_name("run_potrs.py"), 2, "potrs")
+
+
+@pytest.mark.mpmd
+def test_potrs_4gpu():
+    run_mpmd_script(Path(__file__).with_name("run_potrs.py"), 4, "potrs")
